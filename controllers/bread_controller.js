cc04a7bac0c69e4ce661ae1828dbee784307c37a
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-03-07 19:17:06 -0800

    updated to call upon indexes

---
+++ controllers/bread_controller.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const breads = express.Router()
+const Bread = require('../models/bread.js')
+
+// INDEX
+breads.get('/', (req, res) => {
+  res.send(Bread)
+})
+
+// SHOW
+breads.get('/:arrayIndex', (req, res) => {
+  const pickedBread = Bread[Number(req.params.arrayIndex)]
+})
+  res.send(pickedBread)
+})
+
+module.exports = breads
--- server.js
+++ server.js
@@ -1,6 +1,21 @@
+// DEPENDENCIES
 const express = require('express')
 
 // CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
-console.log(PORT)
+const app = express()
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('Welcome to an Awesome App about Breads')
+  })
+  
+  // Breads
+  const breadsController = require('./controllers/bread_controller.js')
+  app.use('/breads', breadsController)
+
+// LISTEN
+app.listen(PORT, () => {
+  console.log('nomming at port', PORT);
+})
