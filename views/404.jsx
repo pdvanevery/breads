ea76dbd8d50c865f44c92999afd3ab7f37f36360
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-03-09 19:28:28 -0800

    updated 404 so it sends a link back to home page

---
+++ views/404.jsx
@@ -0,0 +1,13 @@
+const React = require('react')
+const Default = require('./layouts/default')
+
+function FourOfour() {
+    return (
+        <div>
+            <p>You've reached the wrong page!</p>
+            <a href="/breads">Go home!</a>
+        </div>
+    )
+}
+
+module.exports = FourOfour
