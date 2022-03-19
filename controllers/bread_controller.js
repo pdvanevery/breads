83a139bf8db1808ecdc7bcd9b892cd852e3c795d
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-03-19 13:05:49 -0700

    updated the edit and delete functions to make changes in the database

--- controllers/bread_controller.js
+++ controllers/bread_controller.js
@@ -17,10 +17,12 @@ breads.get('/new', (req, res) => {
 })
 
 // EDIT  ex: breads/2/edit
-breads.get('/:indexArray/edit', (req, res) => {
-  res.render('edit', {
-    bread: Bread[req.params.indexArray],
-    index: req.params.indexArray
+breads.get('/:id/edit', (req, res) => {
+  Bread.findById(req.params.id).then(foundBreads => {
+    res.render('edit' , {
+      bread: foundBreads
+
+    })
   })
 })
 
@@ -54,20 +56,23 @@ breads.post('/', (req, res) => {
 })
 
 // DELETE breads/:id
-breads.delete('/:indexArray', (req, res) => {
-  Bread.splice(req.params.indexArray, 1)
-  res.status(303).redirect('/breads')
+breads.delete('/:id', (req, res) => {
+  Bread.findByIdAndDelete(req.params.id).then(deletedBread => {
+    res.status(303).redirect('/breads')
+  })
 })
   
 // UPDATE breads/:id
-breads.put('/:arrayIndex', (req, res) => {
+breads.put('/:id', (req, res) => {
   if(req.body.hasGluten === 'on'){
     req.body.hasGluten = true
   } else {
     req.body.hasGluten = false
   }
-  Bread[req.params.arrayIndex] = req.body
-  res.redirect(`/breads/${req.params.arrayIndex}`)
+  Bread.findByIdAndUpdate(req.params.id, req.body, { new: true }).then(updateBread => {
+    console.log(updateBread);
+    res.redirect(`/breads/${req.params.id}`)
+  })
 })
 
 
